283e42ec847cdbe9fbd3f07ae550580afac57e97
Hayk.dadian <hayko.dadyan.99@mail.ru>
2017-12-12 20:08:46 +0400

    Initial commit

---
+++ src/main/java/com/haybook/HaybookApplication.java
@@ -0,0 +1,12 @@
+package com.haybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaybookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaybookApplication.class, args);
+	}
+}
