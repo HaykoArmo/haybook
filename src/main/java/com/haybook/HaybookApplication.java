7ddc985291f8db40d1958873570f7b4acd764651
Hayk.dadian <hayko.dadyan.99@mail.ru>
2017-12-14 21:39:39 +0400

    \gides

--- src/main/java/com/haybook/HaybookApplication.java
+++ src/main/java/com/haybook/HaybookApplication.java
@@ -1,13 +1,34 @@
 package com.haybook;
 
+import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
-public class HaybookApplication {
+public class HaybookApplication implements ApplicationContextAware {
+    private ApplicationContext applicationContext;
 
-	public static void main(String[] args) {
-		SpringApplication.run(HaybookApplication.class, args);
-	}
+    @PostConstruct
+    void startup() {
+        DbConnection dbConnectionImpl = (DbConnection) applicationContext.getBean("dbConnectionImpl");
+        dbConnectionImpl.connect("adgh456122156");
+        System.out.println("Starting");
+        System.out.println(dbConnectionImpl);
+        DbConnection dbConnectionImpl2 = (DbConnection) applicationContext.getBean("dbConnectionImpl");
+        System.out.println(dbConnectionImpl2);
 
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaybookApplication.class, args);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
 }
